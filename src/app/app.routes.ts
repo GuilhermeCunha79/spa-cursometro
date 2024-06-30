ed4b7195e77b7bb073aeeb0e8787f48afa4855de
Guilherme Cunha <guilhermecunha1@outlook.pt>
2024-06-30 20:43:08 +0000

    Add calculo-nota component and some html

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import {RouterModule, Routes} from '@angular/router';
+import {CalculoNotaComponent} from "./calculo-nota/calculo-nota.component";
+import {NgModule} from "@angular/core";
+
+export const routes: Routes = [
+  {path: '', component: CalculoNotaComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
+
---
+++ src/app/calculo-nota/calculo-nota.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-calculo-nota',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculo-nota.component.html',
+  styleUrl: './calculo-nota.component.css'
+})
+export class CalculoNotaComponent implements OnInit {
+  ngOnInit(): void {
+  }
+}
